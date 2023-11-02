6ed65790d3dd7fe370464785a7a060eb2f666348
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2023-11-02 15:30:13 +0700

    feat: navbar and login pages

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,162 @@
+'use client'
+
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Popover,
+  PopoverTrigger,
+  useColorModeValue,
+  useDisclosure,
+} from '@chakra-ui/react'
+import {
+  HamburgerIcon,
+  CloseIcon,
+} from '@chakra-ui/icons'
+import NavItem from '../interfaces/NavItem'
+
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'Transfer',
+    href: 'Transfer',
+  },
+  {
+    label: 'History',
+    href: 'History',
+  },
+  {
+    label: 'Promo',
+    href: 'Promo',
+  },
+]
+
+export default function Navbar() {
+  const { isOpen, onToggle } = useDisclosure()
+
+  return (
+    <Box>
+      <Flex
+        bg={useColorModeValue('white', 'gray.800')}
+        color={useColorModeValue('gray.600', 'white')}
+        minH={'60px'}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={'solid'}
+        borderColor={useColorModeValue('gray.200', 'gray.900')}
+        align={'center'}>
+        <Flex
+          flex={{ base: 1, md: 'auto' }}
+          ml={{ base: -2 }}
+          display={{ base: 'flex', md: 'none' }}>
+          <IconButton
+            onClick={onToggle}
+            icon={isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />}
+            variant={'ghost'}
+            aria-label={'Toggle Navigation'}
+          />
+        </Flex>
+        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+          <Flex display={{ base: 'none', md: 'flex' }}>
+            <DesktopNav />
+          </Flex>
+        </Flex>
+
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={'flex-end'}
+          direction={'row'}
+          spacing={6}>
+          <Button
+            as={'a'}
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            fontWeight={600}
+            color={'white'}
+            bg={'pink.400'}
+            href={'Login'}
+            _hover={{
+              bg: 'pink.300',
+            }}>
+            Sign In
+          </Button>
+        </Stack>
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav />
+      </Collapse>
+    </Box>
+  )
+}
+
+const DesktopNav = () => {
+  const linkColor = useColorModeValue('gray.600', 'gray.200')
+  const linkHoverColor = useColorModeValue('gray.800', 'white')
+
+  return (
+    <Stack direction={'row'} spacing={4}>
+      {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={'hover'} placement={'bottom-start'}>
+            <PopoverTrigger>
+              <Box
+                as="a"
+                p={2}
+                href={navItem.href ?? '#'}
+                fontSize={'sm'}
+                fontWeight={500}
+                color={linkColor}
+                _hover={{
+                  textDecoration: 'none',
+                  color: linkHoverColor,
+                }}>
+                {navItem.label}
+              </Box>
+            </PopoverTrigger>
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  )
+}
+
+const MobileNav = () => {
+  const loginNav : NavItem = {label: 'Login', href: 'Login'};
+  return (
+    <Stack bg={useColorModeValue('white', 'gray.800')} p={4} display={{ md: 'none' }}>
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+      <MobileNavItem key={loginNav.label} {...loginNav} />
+    </Stack>
+  )
+}
+
+const MobileNavItem = ({ label, href }: NavItem) => {
+  return (
+    <Stack spacing={4}>
+      <Box
+        py={2}
+        as="a"
+        href={href ?? '#'}
+        justifyContent="space-between"
+        alignItems="center"
+        _hover={{
+          textDecoration: 'none',
+        }}>
+        <Text fontWeight={600} color={useColorModeValue('gray.600', 'gray.200')}>
+          {label}
+        </Text>
+      </Box>
+    </Stack>
+  )
+}
---
+++ src/interfaces/NavItem.ts
@@ -0,0 +1,6 @@
+
+export default interface NavItem {
+    label: string
+    href?: string
+}
+  
