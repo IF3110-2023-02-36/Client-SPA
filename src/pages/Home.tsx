dd21e699788854bbf03809c0ff1d3f575d47d4e4
NaufalArdanni <54439087+NaufalArdanni@users.noreply.github.com>
2023-11-16 13:45:57 +0700

    feat: home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,25 @@
-// TODO : 
+import { Box, Heading, Text, Button, useBreakpointValue } from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
+
+export default function Home() {
+    const headingSize = useBreakpointValue({ base: 'xl', md: '2xl' });
+    const textSize = useBreakpointValue({ base: 'md', md: 'xl' });
+    const buttonSize = useBreakpointValue({ base: 'md', md: 'lg' });
+
+    return (
+        <>
+            <Navbar />
+            <Box w="100%" h="100vh" px={{ base: 4, md: 0 }} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                <Heading as="h1" size={headingSize} mb={6}>
+                    Welcome to the Courier App!
+                </Heading>
+                <Text fontSize={textSize} mb={6}>
+                    Manage your orders all in one place
+                </Text>
+                <Button colorScheme="teal" size={buttonSize} onClick={() => { window.location.href = '/AvailableOrder' }}>
+                    View available orders
+                </Button>
+            </Box>
+        </>
+    );
+}
